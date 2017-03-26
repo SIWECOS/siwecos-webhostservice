cbae72de8135568c7aaa783819591df630793792
Benjamin Trenkle <benjamin.trenkle@wicked-chick.de>
2017-03-26 17:05:38 +0200

    Readd base template

---
+++ resources/views/layouts/base.blade.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="{{ config('app.locale') }}">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>CMS-Garden - {{ config('app.name', 'SiWeCos') }}</title>
+
+        <!-- Fonts -->
+        <link href="{{ asset('css/app.min.css') }}" rel="stylesheet" type="text/css">
+    </head>
+    <body>
+        <div class="full-height">
+            <div class="container content">
+				 @yield('content')
+            </div>
+        </div>
+		<script src="{{ asset('js/app.js') }}"></script>
+    </body>
+</html>
