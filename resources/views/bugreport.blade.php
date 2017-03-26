b8d35fc54bb30f5ac85a32febe13de78c0cb9c07
Florian Häusler <florian@hr-it-solutions.com>
2017-03-26 13:47:26 +0200

    Added form basic structure

--- resources/views/bugreport.blade.php
+++ resources/views/bugreport.blade.php
@@ -8,7 +8,15 @@
 <html>
 <body>
 <form>
-
+    <h1>Submit a bug</h1>
+    <form action="/bugreport" method="post">
+        {!! csrf_field() !!}
+        <div class="form-group">
+            <label for="title">Title</label>
+            <input type="text" class="form-control" id="title" name="title" placeholder="Title">
+        </div>
+        <button type="submit" class="btn btn-default">Submit</button>
+    </form>
 </form>
 </body>
 </html>
