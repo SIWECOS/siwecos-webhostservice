612d1536fe1b747f73b555aba509a2f3b4f02ccb
Florian Häusler <florian@hr-it-solutions.com>
2017-03-26 16:38:03 +0200

    Addded bugreport form structure

--- resources/views/bugreport.blade.php
+++ resources/views/bugreport.blade.php
@@ -9,12 +9,51 @@
 <body>
 <form>
     <h1>Submit a bug</h1>
-    <form action="/bugreport" method="post">
+    <form action="/bugreport" method="post" name="bugreport">
         {!! csrf_field() !!}
-        <div class="form-group">
-            <label for="title">Title</label>
-            <input type="text" class="form-control" id="title" name="title" placeholder="Title">
-        </div>
+        <fieldset>
+            <div class="form-group">
+                <label for="email">E-Mail</label>
+                <input type="email" class="form-control" id="email" name="email" placeholder="1.0">
+            </div>
+            <div class="form-group">
+                <label for="application">Application</label>
+                <select name="application" id="application">
+                    @foreach ($applications as $application)
+                        <option value="{{ $application->value }}">{{ $application->name }}</option>
+                    @endforeach
+                </select>
+                <label for="version">Version</label>
+                <input type="text" class="form-control" id="version" name="version" placeholder="1.0">
+            </div>
+            <div class="form-group">
+                <label for="typeof">Type of</label>
+                <input type="text" class="form-control" id="typeof" name="typeof" placeholder="1.0">
+            </div>
+            <div class="form-group">
+                <label for="typeof">Type of</label>
+                <input type="text" class="form-control" id="typeof" name="typeof" placeholder="1.0">
+            </div>
+            <div class="form-group">
+                <label for="typeof">Exploid</label>
+                <input type="text" class="form-control" id="typeof" name="typeof" placeholder="1.0">
+            </div>
+            <div class="form-group">
+                <label for="report">Report</label>
+                <textarea class="form-control" id="report" name="report" ></textarea>
+            </div>
+            <button>Generate preview</button>
+        </fieldset>
+        <fieldset>
+            <div class="form-group">
+                <label for="clipboard">Clipboard</label>
+                <textarea class="form-control" id="clipboard" name="clipboard" ></textarea>
+            </div>
+            <div class="form-group">
+                <label for="clipboard">signed-email</label>
+                <textarea class="form-control" id="signed-email" name="signed-email" ></textarea>
+            </div>
+        </fieldset>
         <button type="submit" class="btn btn-default">Submit</button>
     </form>
 </form>
