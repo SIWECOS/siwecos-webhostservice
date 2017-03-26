93257bc2dc259574da03b11bb9ff2864ea49d954
Benjamin Trenkle <benjamin.trenkle@wicked-chick.de>
2017-03-26 20:35:58 +0200

    Add header navbar

--- resources/views/layouts/base.blade.php
+++ resources/views/layouts/base.blade.php
@@ -5,13 +5,63 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>CMS-Garden - {{ config('app.name', 'SiWeCos') }}</title>
+        <title>CMS-Garden - {{ config('app.name', 'SIWECOS') }}</title>
 
         <!-- Fonts -->
         <link href="{{ asset('css/app.min.css') }}" rel="stylesheet" type="text/css">
     </head>
     <body>
         <div class="full-height">
+			<nav class="navbar navbar-default navbar-static-top">
+				<div class="container">
+					<div class="navbar-header">
+
+						<!-- Collapsed Hamburger -->
+						<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#base-navbar-collapse">
+							<span class="sr-only">Toggle Navigation</span>
+							<span class="icon-bar"></span>
+							<span class="icon-bar"></span>
+							<span class="icon-bar"></span>
+						</button>
+
+						<!-- Branding Image -->
+						<a class="navbar-brand" href="{{ url('/') }}">
+							<img class="img-responsive" alt="{{ config('app.name', 'SIWECOS') }}" src="{{ asset('images/siwecos_logo.png') }}">
+						</a>
+					</div>
+
+					<div class="collapse navbar-collapse" id="base-navbar-collapse">
+						<!-- Right Side Of Navbar -->
+						<ul class="nav navbar-nav navbar-right">
+							<!-- Authentication Links -->
+							@if (Auth::guest())
+								<li><a href="{{ route('login') }}">Login</a></li>
+								<li><a href="{{ route('register') }}">Register</a></li>
+							@else
+								<li class="dropdown">
+									<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
+										{{ Auth::user()->name }} <span class="caret"></span>
+									</a>
+
+									<ul class="dropdown-menu" role="menu">
+										<li>
+											<a href="{{ route('logout') }}"
+												onclick="event.preventDefault();
+														 document.getElementById('logout-form').submit();">
+												Logout
+											</a>
+
+											<form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+												{{ csrf_field() }}
+											</form>
+										</li>
+									</ul>
+								</li>
+							@endif
+						</ul>
+					</div>
+				</div>
+			</nav>
             <div class="container content">
 				 @yield('content')
             </div>
